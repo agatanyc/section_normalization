ea55ffe0722190379e905c70d033d4f08e3f2bf5
Agata Filho <agata78@gmail.com>
2016-12-03 19:11:19 -0500

    Move the model class to the main file.

--- normalizer.py
+++ normalizer.py
@@ -1,14 +1,42 @@
-from model import Manifest, init_db
 from string import ascii_uppercase
 from sqlalchemy_utils import database_exists
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
 
+
+Base = declarative_base()
+
+class Manifest(Base):
+
+    __tablename__ = 'manifest'
+
+    primary_key = Column(Integer, autoincrement=True, primary_key=True)
+    # section_id,section_name,row_id,row_name
+    section_id = Column(Integer)
+    section_name = Column(String(40))
+    row_id = Column(Integer)
+    row_name = Column(String(40))
+
+def get_engine():
+    return create_engine('sqlite:///manifest_data.db')
+
+def init_db():
+    engine = get_engine()
+    Base.metadata.create_all(engine)
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    return DBSession()
+
+def create_all():
+    Base.metadata.create_all(get_engine())
 
 session = init_db()
 
 class Normalizer(object):
     def __init__(self):
-        pass
-
+        create_all()
 
     def read_manifest(self, manifest):
         """Read a manifest file and populate the database.
